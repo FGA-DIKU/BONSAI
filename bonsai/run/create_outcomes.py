5d4e881eaa178dd4bbf13f8e737ffce89827a6d3
LlambiasMBP <llambias@live.com>
2026-03-03 09:50:43 +0100

    merge

--- bonsai/run/create_outcomes.py
+++ bonsai/run/create_outcomes.py
@@ -32,7 +32,7 @@ def main(cfg: DictConfig) -> None:
     exclude = outcome.exclude
     index = outcome.index
     censor = outcome.censor
-
+    print(outcome, outcome.censor, outcome.censor.get("hour_shift"))
     # TODO: Single file for each outcome or one for each split_outcome combination?
     # TODO: Implement some logging?
     all_outcomes = pd.DataFrame()
@@ -45,91 +45,39 @@ def main(cfg: DictConfig) -> None:
 
             df = df.dropna(subset=["subject_id", "time", "code"])
 
-            for outcome in cfg.outcomes:
-                match = cfg.outcomes[outcome].match
-                exclude = cfg.outcomes[outcome].exclude
-                index = cfg.outcomes[outcome].index
-                censor = cfg.outcomes[outcome].censor
+            # Find rows matching exclude.conditions and exclude them
+            if exclude is not None:
+                exclude_df = find(df, exclude.conditions, exclude.dependence)
+                df = df[~df["subject_id"].isin(exclude_df["subject_id"])]
 
-                # Find rows matching exclude.conditions and exclude them
-                if exclude is not None:
-                    exclude_df = find(df, exclude.conditions, exclude.dependence)
-                    df = df[~df["subject_id"].isin(exclude_df["subject_id"])]
+            # Find the outcomes matching match.conditions
+            outcomes = find(df, match.conditions, match.dependence)
+            outcomes = (
+                df[["subject_id"]]
+                .drop_duplicates()
+                .merge(outcomes, on="subject_id", how="left")
+            )
 
-                # Find the outcomes matching match.conditions
-                outcomes = find(df, match.conditions, match.dependence)
-                outcomes = (
-                    df[["subject_id"]]
-                    .drop_duplicates()
-                    .merge(outcomes, on="subject_id", how="left")
-                )
-                assert len(outcomes) == df["subject_id"].nunique()
+            assert len(outcomes) == df["subject_id"].nunique()
 
-                outcomes = outcomes.drop(columns="code").rename(
-                    columns={"time": "outcome_date"}
-                )
-                outcomes["index_date"] = set_dates(
-                    date_type=index.type,  # Absolute/relative
-                    outcome_dates=outcomes["outcome_date"],  # Required for relative
-                    hour_shift=index.get("hour_shift"),  # Required for relative
-                    date=index.get("date"),  # Required for absolute
-                )
+            outcomes = outcomes.drop(columns="code").rename(
+                columns={"time": "outcome_date"}
+            )
+            outcomes["index_date"] = set_dates(
+                date_type=index.type,  # Absolute/relative
+                outcome_dates=outcomes["outcome_date"],  # Required for relative
+                hour_shift=index.get("hour_shift"),  # Required for relative
+                date=index.get("date"),  # Required for absolute
+            )
+            outcomes["censor_date"] = set_dates(
+                date_type=censor.type,  # Absolute/relative
+                outcome_dates=outcomes["outcome_date"],  # Required for relative
+                hour_shift=censor.get("hour_shift"),  # Required for relative
+                date=censor.get("date"),  # Required for absolute
+            )
+            all_outcomes = pd.concat((all_outcomes, outcomes))
 
-                outcomes["censor_date"] = set_dates(
-                    date_type=censor.type,  # Absolute/relative
-                    outcome_dates=outcomes["outcome_date"],  # Required for relative
-                    hour_shift=censor.get("hour_shift"),  # Required for relative
-                    date=censor.get("date"),  # Required for absolute
-                )
-                all_outcomes[outcome] = pd.concat((all_outcomes[outcome], outcomes))
-                print(outcomes)
-
-    path_outcomes.mkdir(parents=True, exist_ok=True)
-    for outcome, df_out in all_outcomes.items():
-        # TODO: implement logger?
-        print(f"Saving {outcome} to {path_outcomes / outcome}.parquet")
-        # print(df_out)
-        df_out.to_parquet(path_outcomes / f"{outcome}.parquet")
-
-
-def find(df, conditions: List, dependence: Literal["independent", "dependent"]):
-    """Returns the first row (priority based on condition order) for each patient that matches the conditions"""
-    # Initialization
-    df["_prio"] = pd.Series()
-    masks = False
-    subject_sets = []
-
-    for i, cond in enumerate(conditions):
-        cond_mask = df[cond["col"]].isin(cond["vals"])  # Rows that meet condition
-        masks |= cond_mask  # OR operation
-        df["_prio"] = df["_prio"].mask(
-            cond_mask, i
-        )  # Set priority (to take first row later)
-        subject_sets.append(
-            set(df.loc[cond_mask, "subject_id"])
-        )  # Get subject that match condition
-
-    # Toggle betweens any or all conditions met
-    if dependence == "independent":
-        matched_subjects = set.union(*subject_sets)  # Any condition met
-    elif dependence == "dependent":  # TODO: Implement time_window
-        matched_subjects = set.intersection(*subject_sets)  # All conditions met
-    else:
-        raise ValueError(
-            f"Dependence can only be [independent, dependent], not {dependence}"
-        )
-
-    # Get matched subjects AND rows
-    res = df[df["subject_id"].isin(matched_subjects) & masks]
-
-    # Take first row based on `conditions` ordering
-    res = (
-        res.sort_values("_prio")
-        .groupby("subject_id", sort=False, as_index=False)
-        .first()
-    )
-    res = res.drop(columns="_prio")
-    return res
+    all_outcomes.to_parquet(save_path)
 
 
 if __name__ == "__main__":
