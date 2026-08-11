800208d0b90cc84b2058d02665101786017e918b
Mikkelfo <mikk528a@gmail.com>
2026-08-11 13:58:40 +0200

    Seperated SDPA and flash into two forward passes

--- bonsai/modules/networks/bonsai_nets.py
+++ bonsai/modules/networks/bonsai_nets.py
@@ -69,6 +69,16 @@ class BonsaiBase(nn.Module):
         }
 
     def forward(self, batch):
+        if self.hparams["attn_type"] == "flash":
+            return self.forward_flash(batch)
+        elif self.hparams["attn_type"] == "sdpa" and not self.hparams["causal"]:
+            return self.forward_sdpa(batch)
+        else:
+            raise ValueError(
+                f"Invalid attention type: {self.hparams['attn_type']}. Only 'flash' and non-causal 'sdpa' are supported."
+            )
+
+    def forward_sdpa(self, batch):
         x = self.embeddings(
             code=batch["code"],
             age=batch["age"],
@@ -78,37 +88,38 @@ class BonsaiBase(nn.Module):
 
         x = self.drop(x)
 
-        # FlashAttention requires unpadded input and cu_seqlens for variable-length sequences
-        if self.hparams["attn_type"] == "flash":
-            batch_size, padded_seqlen = x.shape[:2]
-            (x, indices, cu_seqlens, max_seqlen, _) = unpad_input(
-                x, batch["attention_mask"]
-            )  # x: (batch, padded_seqlen, hidden_size) -> (total_valid_tokens, hidden_size)
-            attn_mask = None  # Flash attention does not require an attention mask
-        # SDPA requires a broadcasted attention mask
-        elif self.hparams["attn_type"] == "sdpa" and not self.hparams["causal"]:
-            attn_mask = batch["attention_mask"][:, None, None, :]
-            cu_seqlens, max_seqlen = None, None
-
-        else:
-            raise ValueError(
-                "Invalid attention setup. Only non-causal SDPA and FlashAttention are supported."
-            )
+        attn_mask = batch["attention_mask"][:, None, None, :]
 
         for layer in self.layers:
-            x = layer(
-                x,
-                attn_mask=attn_mask,
-                cu_seqlens=cu_seqlens,
-                max_seqlen=max_seqlen,
-            )
+            x = layer(x, attn_mask=attn_mask)
 
         x = self.layernorm(x)
 
-        if self.hparams["attn_type"] == "flash":
-            x = pad_input(
-                x, indices, batch_size, padded_seqlen
-            )  # x: (total_valid_tokens, hidden_size) -> (batch, padded_seqlen, hidden_size)
+        return x
+
+    def forward_flash(self, batch):
+        x = self.embeddings(
+            code=batch["code"],
+            age=batch["age"],
+            abspos=batch["abspos"],
+            segment=batch["segment"],
+        )
+
+        x = self.drop(x)
+
+        batch_size, padded_seqlen = x.shape[:2]
+        (x, indices, cu_seqlens, max_seqlen, _) = unpad_input(
+            x, batch["attention_mask"]
+        )  # x: (batch, padded_seqlen, hidden_size) -> (total_valid_tokens, hidden_size)
+
+        for layer in self.layers:
+            x = layer(x, cu_seqlens=cu_seqlens, max_seqlen=max_seqlen)
+
+        x = self.layernorm(x)
+
+        x = pad_input(
+            x, indices, batch_size, padded_seqlen
+        )  # x: (total_valid_tokens, hidden_size) -> (batch, padded_seqlen, hidden_size)
 
         return x
 
