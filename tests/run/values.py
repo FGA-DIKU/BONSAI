ea7d269db42bc02f0429d212bc536206419eff59
LlambiasMBP <llambias@live.com>
2026-08-13 09:37:37 +0200

    .

--- tests/run/values.py
+++ tests/run/values.py
@@ -12,29 +12,29 @@ correct = subprocess.run(
     text=True,
 )
 
-# correct = subprocess.run(
-#    [
-#        "python",
-#        "bonsai/run/create_outcome.py",
-#        "--config-name",
-#        "examples/example_outcome_val",
-#        "dataset=lab_correlated_MEDS_data_not_fused",
-#    ],
-#    check=True,
-#    text=True,
-# )
+correct = subprocess.run(
+    [
+        "python",
+        "bonsai/run/create_outcome.py",
+        "--config-name",
+        "examples/example_outcome_val",
+        "dataset=lab_correlated_MEDS_data_not_fused",
+    ],
+    check=True,
+    text=True,
+)
 
-# correct = subprocess.run(
-#    [
-#        "python",
-#        "bonsai/run/pretrain.py",
-#        "--config-name",
-#        "examples/example_pretrain_val",
-#        "dataset=lab_correlated_MEDS_data_not_fused",
-#    ],
-#    check=True,
-#    text=True,
-# )
+correct = subprocess.run(
+    [
+        "python",
+        "bonsai/run/pretrain.py",
+        "--config-name",
+        "examples/example_pretrain_val",
+        "dataset=lab_correlated_MEDS_data_not_fused",
+    ],
+    check=True,
+    text=True,
+)
 #
 # correct = subprocess.run(
 #    [
